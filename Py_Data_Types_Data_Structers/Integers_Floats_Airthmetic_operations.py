d40ade7e63d6416e2ad46735d229a63f23556381
legionJP <jjpsingh18@gmail.com>
2024-02-24 23:51:17 +0530

    updating the list, tuple, dict and set

--- Py_Data_Types_Data_Structers/Integers_Floats_Airthmetic_operations.py
+++ Py_Data_Types_Data_Structers/Integers_Floats_Airthmetic_operations.py
@@ -1,1 +1,35 @@
 # Airthmetic_Operations 
+# Addition # Division # Subtraction #Multiplication
+# Floor Divison
+print(3//2)
+
+#Exponent 
+print(3**2)
+
+#Modulus 
+print(3%2)
+print(5%2)
+print(2%2)
+
+num =1 
+num += 10
+print(num)
+
+print(abs(-10))
+
+print(round(4.567,2)) #round to the two digit 
+
+# Comparison Operator 
+
+num1 = 2
+num2 = 3
+print(num1 == num2)
+print(num1!=num2)
+print(num1<num2)
+
+#Cashing in the Python
+num_1 ='100'
+num_2 = '200'
+print(num_1+ num_2) #Unexpected result
+
+print(int(num_1)+int(num_2))
