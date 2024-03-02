29eee375cb6044937a48e1c4a24b740f213c7428
legionJP <jjpsingh18@gmail.com>
2024-03-02 23:21:25 +0530

    Adding the py Packages and IDE, env note.

---
+++ Python_IDEs_Packaging/Virtualenv.py
@@ -0,0 +1,39 @@
+'''
+1. pip install virtualenv
+
+mkdir "dr name "
+cd directory
+virtualenv project1_env # name of the project environment 
+
+source project1_env/bin/activate  / for activating the env.
+
+which pip \\ or which python.
+
+pip list .
+
+pip install (packages name ).
+
+pip freeze --local > requirement.txt # it takes only local dependencies
+
+cat requirement.txt .
+
+//getting out of the environment
+deactivate
+
+
+rm -rf project1_env  #deleting the environment permanetly
+
+\\ making the env with the project name
+
+virtualenv -p /usr/bin/python3.10 project2_env
+ # activating 
+
+ source py26_env/bin/activate .
+
+ /you can switch between poject by activaiting and deactiavate
+
+  
+
+
+
+'''
