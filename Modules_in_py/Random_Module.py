59d5cd00f7d9f7e7f48cfd79af37c7f0824b0f4a
legionJP <legionJP>
2024-03-13 00:40:57 +0530

    Adding the random module code

--- Modules_in_py/Random_Module.py
+++ Modules_in_py/Random_Module.py
@@ -5,19 +5,28 @@ print(value1)
 val_int =random.randint(0,1)
 print(val_int)
 
+# random.choice
+
 greetings = ['Hello' , 'Namaste' , 'Hi', 'Hey' , 'Hola']
 values = random.choice(greetings)
 print(values + ' JP')
+
+# random .choices 
 
 Col_list = ['Red','Black' ,'Blue']
 result = random.choices(Col_list , k= 10) # k = require sequence
 result = random.choices(Col_list ,weights=[18,18,4], k= 10) # 18 is the chances of selection
 print(result)
 
+# .suffle() Method
 
 deck_card = list(range(1,52))
-random.shuffle(deck_card)
+random.shuffle(deck_card)  # it can select duplicate value
 print(deck_card)
 
+
+# .samle method
 hand_card = random.sample(deck_card, k=5) #chooses the unique sample number
 print(hand_card)
+
+
