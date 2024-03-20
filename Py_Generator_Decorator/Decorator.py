0b4b79fe06b2e0d9038ce239457ea9b25bd6bd0e
legionJP <jjpsingh18@gmail.com>
2024-03-20 18:45:34 +0530

    decorator fun concepts

--- Py_Generator_Decorator/Decorator.py
+++ Py_Generator_Decorator/Decorator.py
@@ -1,1 +1,31 @@
+def outer_fun(msg):
+   # message = msg
 
+    def inner_function():
+        print(msg)
+    return inner_function
+
+#my_func=outer_fun() 
+func1 = outer_fun('First')
+func2 = outer_fun('Second')
+
+func1()
+func2()
+
+#Decorator 
+'''
+It is just a function that takes another function as a argument and 
+add some kind of functionality and then return other function without altering thesource code 
+'''
+
+def decorater_fun(original_function):
+    def wrapper_function():
+        return original_function()   #This is decorator 
+        #print(message)
+    return wrapper_function
+
+hi_func = outer_fun('Hi')
+bye_func = outer_fun('Bye')
+
+hi_func()
+bye_func()
