755c92b1879e08ead65e07806afcc4c614ee936f
legionJP <jjpsingh18@gmail.com>
2024-03-22 04:07:32 +0530

    Updating Decorator

--- Py_Generator_Decorator/Decorator.py
+++ Py_Generator_Decorator/Decorator.py
@@ -19,13 +19,32 @@ add some kind of functionality and then return other function without altering t
 '''
 
 def decorater_fun(original_function):
-    def wrapper_function():
-        return original_function()   #This is decorator 
+    def wrapper_function(*args,**kwargs):
+        print('This is excecuting before the wrapper {}'.format(original_function.__name__))
+        return original_function(*args,**kwargs)   #This is decorator 
         #print(message)
     return wrapper_function
 
-hi_func = outer_fun('Hi')
-bye_func = outer_fun('Bye')
+# def display():
+#     print("Display func ran")
+# decorater_display = decorater_fun(display)
+# decorater_display() #Equal to the wrapper function 
 
-hi_func()
-bye_func()
+#Decorator expression type = " @decorater_fun = decorator_display = decorator_fun(display) are the same"
+
+# or Using the Function as a Decorator with multiple func 
+@decorater_fun
+def display():
+    print("Display func ran")
+
+#display()
+
+@decorater_fun
+def display_info(name ,age):
+    print('The info about person name and age is ({} ,{})'.format(name,age))
+display_info('Jay',23) 
+
+display()
+ 
+
+ #Using the Classes as aDecorator
