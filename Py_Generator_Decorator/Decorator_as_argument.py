7052fecbe636a128fe4c101e70871a1aca844f47
legionJP <jjpsingh18@gmail.com>
2024-03-27 05:47:51 +0530

    a function for  how to use the decorator as a argument

---
+++ Py_Generator_Decorator/Decorator_as_argument.py
@@ -0,0 +1,24 @@
+#Argument for decorator or customizeable layer to decorator 
+
+def prefix_decorator(prefix):
+   
+    def decorator_fun(origi_func):
+       def wrapper_fun(*args,**kwargs):
+           print(prefix,'Executeed before',origi_func.__name__)
+           result = origi_func(*args, **kwargs)
+           print(prefix,'Execcuted after ', origi_func.__name__ ,'\n')
+           return result
+       return wrapper_fun
+    return decorator_fun
+      
+     
+@prefix_decorator('Texting Log:')        #in this decoraotr we can pass on the our argument 
+def display_fun(name,age):
+   print('display info ran with ({}, {})'.format(name,age))
+
+display_fun('AJ', 24)
+display_fun('vK', 23)
+
+
+
+
