8bc4a55aa1d2e2e2b6d8a9e8d4d984defdb3ec33
legionJP <jjpsingh18@gmail.com>
2024-03-28 18:58:45 +0530

    Updating class varriables  and instance method

--- OOPs_in_Python/Class_Varriables_&_Instance_method.py
+++ OOPs_in_Python/Class_Varriables_&_Instance_method.py
@@ -49,3 +49,68 @@ print(Poha.items)
 #Print the instance method  Contents over object Pizaa or poha 
 
 print(Pizza.contents())
+
+
+#_------------------------------------------------------------------------------
+# 2. Class Varriables
+#------------------------------------------------------------------------------
+
+#Instance varriable should be unique for each instance 
+#But class varriables are same for each instance
+
+class Employee:  #class is the blueprint for creating the instances
+   
+   num_of_emolyee =0
+   raise_amount = 1.04
+   def __init__(self,first,last,pay): #first instance is self
+        self.first =first      # this method called intialize and in other language  it called as constructer
+        self.last = last
+        self.pay =pay
+        self.email = first + '.' + last + '@email.com'
+
+        Employee.num_of_emolyee += 1
+   
+   def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+   def apply_raise(self): #creating the method for raise in salary
+        #self.pay = int(self.pay *1.04) #??? 1....
+        #Acessing the class var need to access trhough class or from instance of class
+        #self.pay = int(self.pay *Employee.raise_amount) #Accessing  var. through class
+        self.pay = int(self.pay *self.raise_amount) #access trhough instance, this allows to changes the amt for single instance
+
+emp1 = Employee('JP', 'Pal',45000)
+emp2 = Employee('AK','asd', 56432)  
+
+print(emp1.pay)
+emp1.apply_raise()
+print(emp1.pay)
+
+#Employee.raise_amount =1.05 #change for emp1 
+emp1.raise_amount =1.05 # changes in whole instances
+
+print(Employee.raise_amount)
+print(emp1.raise_amount)
+print(emp2.raise_amount)
+
+print(Employee.num_of_emolyee)
+
+#Printing the namespace of the instnace emp1
+print(emp1.__dict__)
+print(Employee.__dict__) #class contian the raise_amount attribute so the instance access this from class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
