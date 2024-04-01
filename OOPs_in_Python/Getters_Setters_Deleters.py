9d14e3b3ddc22d4449ee587cd04e4f6ab247be6c
legionJP <jjpsingh18@gmail.com>
2024-04-01 18:48:57 +0530

    Update in file

--- OOPs_in_Python/Getters_Setters_Deleters.py
+++ OOPs_in_Python/Getters_Setters_Deleters.py
@@ -1,3 +1,33 @@
 
 #Property Decorators - Getters , Setters , Deleters : 
 
+class Employee:  #class is the blueprint for creating the instances
+   
+   num_of_emolyee =0
+   raise_amount = 1.04
+
+   def __init__(self,first,last):  # METHOD 1
+        self.first =first      
+        self.last = last
+        #self.pay =pay
+        self.email = first + '.' + last + '@email.com'
+
+        Employee.num_of_emolyee += 1
+   
+   def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+emp1 = Employee('JP', 'PAL') 
+
+emp1.first = 'Jay' # this is 
+
+print(emp1.first)
+print(emp1.last)
+print(emp1.email)
+print(emp1.fullname())
+
+
+
+
+
+
