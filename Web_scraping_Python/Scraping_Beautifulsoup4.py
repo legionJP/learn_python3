f5f19f6ce849a005fe4249377413c636a2d3ce82
legionJP <jjpsingh18@gmail.com>
2024-04-02 14:09:25 +0530

    adding sample html file

--- Web_scraping_Python/Scraping_Beautifulsoup4.py
+++ Web_scraping_Python/Scraping_Beautifulsoup4.py
@@ -7,3 +7,12 @@
 
 # pip install requests
 
+from bs4 import BeautifulSoup
+import requests
+
+#paasing the html file 
+with open ('example.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+print(soup.prettify)
+
