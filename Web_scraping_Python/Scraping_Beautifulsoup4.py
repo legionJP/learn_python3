22762b0c6862ac343738739f14b97a59c6e18e56
legionJP <jjpsingh18@gmail.com>
2024-04-03 17:24:27 +0530

    adding the code for the web scrapping

--- Web_scraping_Python/Scraping_Beautifulsoup4.py
+++ Web_scraping_Python/Scraping_Beautifulsoup4.py
@@ -34,7 +34,6 @@ headline = article.h2.a.text
 summary = article.p.text
 print(summary)
 print()
-
 #or 
 
 for article in soup.find_all('div',class_ ='article'):
@@ -52,7 +51,7 @@ for article in soup.find_all('div',class_ ='article'):
 
 #--------------------------------------------------------
     
-
+import csv
 from bs4 import BeautifulSoup
 import requests
 
@@ -62,28 +61,50 @@ soup = BeautifulSoup(source,'lxml')
 print(soup.prettify())
 print()
 
+csv_file = open('cms_scrape.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow([headline,summary])
 
 #---------------------------------------------------------------------------------------
 # scraping the youtube video and id
 #------------------------------------
-article = soup.find('div', class_ = 'sc-bdvvtL lhrfPG') #.p.txt
-print(article.prettify())
+
+# for article in soup.find_all('article'):
+#     headline = article.h2.text
+#     print(headline)
+
+#     summary = article.find('div', class_ = 'entry-content').p.text
+#     print(summary)
+
+#     article = soup.find('div', class_ = 'sc-bdvvtL lhrfPG') #.p.txt
+#     print(article.prettify())
+ 
+#     try:
+#         vid_src = article.find('iframe', class_ = 'youtube-player')['src'] #iframe as a attribute for the src  attribute of that tag
+#         print(vid_src)    
+
+#     #spiliting the id string 
+#         vid_id = vid_src.spilit('/')
+#         print(vid_id)
+#      # as a list the item is hown with the index value 
+#      #link at the 4 index
+
+#         vid_id = vid_id.split('/')[4]
+#         vid_id = vid_id.spilit('?')[0]# spliting based on ? 
+#         print(vid_id) #video id = K6L6KVGG-7o
+
+#         yt_link = f'https://youtube.com/watch?v={vid_id}'
+#         print(yt_link)
+
+#     except Exception as e:
+#         raise e
+#     print()
+
+#     csv_writer.writerow([headline, summary, yt_link])
+#     csv_file.close()
 
 
 
-vid_src = article.find('iframe', class_ = 'youtube-player')['src'] #iframe as a attribute for the src  attribute of that tag
-print(vid_src)    
-
-#spiliting the id string 
-
-vid_id = vid_src.spilit('/')
-print(vid_id)
- # as a list the item is hown with the index value 
-#link at the 4 index
-
-vid_id = vid_id.split('/')[4]
-vid_id = vid_id.spilit('?')[0]
-print(vid_id)
 
 
 
