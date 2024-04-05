94f9553457145f4689370b57f4348a4870d7b573
legionJP <jjpsingh18@gmail.com>
2024-04-05 17:08:14 +0530

    error fixinng

--- JSON_Module_py/json_module.py
+++ JSON_Module_py/json_module.py
@@ -53,9 +53,7 @@ for person in data['people']:
 new_string = json.dumps(data,indent=2, sort_keys= True) #indent method for data formating 
 print(new_string)
 
-#-----------------------------------------------------
 # Loading the  Json file in python object and back in json file 
-#---------------------------------------------------------
 
 import json 
 
@@ -70,10 +68,8 @@ for state in file_data['states']: #acces the state key
     del state['area_codes']
     #print(state) 
 
-    
-#---------------------------------------
 #json.dump and json.dumps method
-#---------------------------------------    
+    
 with open('new_states.json','w') as f:
     json.dump(file_data , f,indent=2)
 
@@ -82,56 +78,11 @@ with open('new_states.json','w') as f:
 #-------------------------------------------
     
 import json   
-import requests
-import urllib.request
 from urllib.request import urlopen
 
-with  requests.get("https://api.currencyfreaks.com/v2.0/rates/latest?apikey=6dc0c80fdc874a7d8bd3136da9d18dfc")as response:
-    source = response.json()
-#print(source)
-
-# with open ('response2.json', 'w') as currency_data:
-#      json.dump(source,currency_data, indent=2)
-
-with open('response2.json') as currency_exchange:
-    currency_data1 = json.load(currency_exchange)
-print(currency_data1)
-print(len(currency_data1))
-
-#------------------------------------------------------
-# If the response is the list than coverting the currency value
-#-----------------------------------------------------
-
-usd_rates = dict()
-for item in currency_data1['rates']['base']:
-    print(item)
-    name = item['resource']['fields']['name']
-    price = item['resource']['fields']['price']
-    usd_rates[name] = price
-    print(name, price)
-
-print(50* float(usd_rates['USD/EUR']))
-
-#-----------------------------------------------------
-
-# resp = requests.get("https://api.currencyfreaks.com/v2.0/rates/latest?apikey=6dc0c80fdc874a7d8bd3136da9d18dfc")
-# print(resp.json())
-# print(type(resp))
-
-# for item in resp['rates']:
-#     print(item)
+with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?fromat=json") as response:
+    source = response.read()
+print(source)
 
 
-# import http.client
-
-# conn = http.client.HTTPSConnection("api.currencyfreaks.com")
-# payload = ''
-# headers = {}
-# conn.request("GET", "/v2.0/convert/latest?from=usd&to=pkr&amount=500&apikey=6dc0c80fdc874a7d8bd3136da9d18dfc", payload, headers)
-# res = conn.getresponse()
-# data = res.read()
-# print(data.decode("utf-8"))
-
-
-
-
+ 
