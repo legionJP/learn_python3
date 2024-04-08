f7f553d0355c7eefeb27fe9be665b7b2d097fc64
legionJP <jjpsingh18@gmail.com>
2024-04-08 16:05:51 +0530

    Adding the SQLite py guide

---
+++ SQLite_python.py/SQLite_db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+#making the connection object
+
+conn =sqlite3.connect(':memory:') #making the in-memory database
+
+
+#for filename db
+db_conn = sqlite3.connect('employee.db')
+
+c= db_conn.cursor()   #creating the cursor for excexuting sql command
+
+# c.execute("""CREATE TABLE employees(
+#           first text,
+#           last text,
+#           pay integer
+# )""")
+
+#adding the data in db table 
+
+# c.execute("INSERT INTO employees VALUES ('Akash','Kumar',45435)")
+
+c.execute("SELECT * FROM employees where last ='Kumar'")
+c.fetchone() #only returne one
+
+# c.fetchmany(5) #number of  rows as a list
+# c.fetchmany() 
+
+db_conn.commit() #commiting the changes
+
+db_conn.close()
