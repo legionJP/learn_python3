ca8f2d72c71f7b5493ea70fd8100e505f3e1a9a7
legionJP <jjpsingh18@gmail.com>
2024-04-10 12:52:12 +0530

    adding the formatter for logging

--- Python_logging/Py_logging_advance.py
+++ Python_logging/Py_logging_advance.py
@@ -19,12 +19,19 @@ fallback to root logger
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 logger =logging.getLogger(__name__)  
 
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 file_handler = logging.FileHandler('sample.log')
 file_handler.setFormatter(formatter)
 
+stream_handler =logging.StreamHandler()   #If stream is not specified, sys.stderr is used.
+stream_handler.setFormatter(formatter)
+
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler) #it show the debug error in the console
+
+
 
 # logging.basicConfig(filename='sample.log',level=logging.INFO,
 #                     format='%(asctime)s:%(levelname)s:%(message)s') #DEBUG is the constant but int in the back ground
@@ -46,25 +53,30 @@ def subtract(x,y):
 def multiply(x,y):
     return x * y
 def divide(x,y):
-    return x / y
+    try:
+        result =x/y
+    except ZeroDivisionError:
+        logger.exception('tried to divide by zero')
+
+    else:
+        return result
 
 num1 = 12
-num2 = 11
+num2 = 0
 add_result = add(num1,num2)
  
 #logger.Warning('Add: {} + {}= {}'.format(num1,num2,add_result)) #its not logging the debug  when the logging config level was not declared 
-logger.info('Add: {} + {}= {}'.format(num1,num2,add_result))
+logger.debug('Add: {} + {}= {}'.format(num1,num2,add_result))
 
 sub_result = subtract(num1,num2)
 #print('Subtracr: {} - {}= {}'.format(num1,num2,sub_result))
-logger.info('Subtract: {} - {}= {}'.format(num1,num2,sub_result))
+logger.debug('Subtract: {} - {}= {}'.format(num1,num2,sub_result))
 
 multi_result = multiply(num1,num2)
 # print('Multiply: {} * {}= {}'.format(num1,num2,multi_result))
-logger.info('Multiply: {} * {}= {}'.format(num1,num2,multi_result))
+logger.debug('Multiply: {} * {}= {}'.format(num1,num2,multi_result))
 
 div_result = divide(num1,num2)
 #print('divide: {} / {}= {}'.format(num1,num2,div_result))
-logger.info('divide: {} / {}= {}'.format(num1,num2,div_result))
+logger.debug('divide: {} / {}= {}'.format(num1,num2,div_result))
 #logging.debug('divide: {} / {}= {}'.format(num1,num2,div_result))
-
