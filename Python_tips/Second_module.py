84a1d938f43e42a4795fe02343e5823ece87de9e
legionJP <jjpsingh18@gmail.com>
2024-04-10 17:09:56 +0530

    adding the __name__ and __main__ for using it in imports

---
+++ Python_tips/Second_module.py
@@ -0,0 +1,9 @@
+import first_module__main__PY
+#py is importing the file and so module is the file name 
+#when we have if statement in the first_module file it is not printing the imported module name
+
+#to run main method in second module, can use this code
+first_module__main__PY.main()
+
+print('second Module name is {}'.format(__name__))
+# second module is __main__ , because the python is runnig the file directly
---
+++ Python_tips/first_module__main__PY.py
@@ -0,0 +1,25 @@
+
+print("this method wil always run ") #bcz this is outside of the main() method 
+#print("the first Module's Name is {}".format(__name__))
+
+#whenever python runs a file before  it sets a few special variable so __name__ is one of them
+# so when python runs a python file dritecly so it says that __name__ variable is eqal to __main__
+# so when  we import the file first_module... than the file name is set to the __name__ variable  
+#that's why module for imported file will be first_Module__main__PY
+
+def main():
+    print("the first Module's Name is {}".format(__name__))
+    
+if __name__ == '__main__':
+    main()
+
+#above if statement is for : 
+#that if the file is directly run by python or being imported  
+#if statement is true than run main() method
+
+#Comment above method and run this 
+
+if __name__ == '__main__':
+    print('Runing directly')
+else:
+    print('Running form import')    
