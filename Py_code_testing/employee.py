71f4761ec7384349aab352cd3797f8390422edac
legionJP <jjpsingh18@gmail.com>
2024-04-12 16:12:39 +0530

    updating employee test code

--- Py_code_testing/employee.py
+++ Py_code_testing/employee.py
@@ -1,3 +1,9 @@
+import requests
+from unittest.mock import patch  #we can use patch as a decorater or as a context manager 
+
+# Patch wil allow us to mock  a object during a test and then the 
+#object is atumatically restored after the test is run 
+
 class Employee:   
    raise_amount = 1.05
    def __init__(self,first,last,pay):  
@@ -16,3 +22,14 @@ class Employee:
    
    def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
+
+
+# Mocking Method : 
+# you want to get some info from web but website is down so your function will fail, so to avoid this 
+
+def monthly_schedule(self, month):
+     response = requests.get(f'http://company.comm/{self.last}/{month}')
+     if response.ok:
+          return response.text
+     else:
+          return 'Bad Requests'
--- Py_code_testing/test_employee.py
+++ Py_code_testing/test_employee.py
@@ -1,17 +1,24 @@
 import unittest
 from employee import Employee
-
+from unittest.mock import patch
+import employee
 class TestEmployee(unittest.TestCase):
 
     #setUP and #tearDown method for avoidinng the repetation in code
+
+    #for class method refer to 'test_Employee2.py'
 
     def setUp(self):
         self.emp_1 = Employee('Abba','Sabba',50000)
         self.emp_2 = Employee('Hamba','Kamba',60000) 
         return super().setUp()
-    
+
     def tearDown(self) -> None:
         return super().tearDown()
+    
+    #Note: In a setUp method you could create the test directory or the test database to hold those 
+    #file , 2. In a teraDown Method you could delete all of those so you could have clean slate for next test.
+
 
     def test_email(self):
         # emp_1 = Employee('Abba','Sabba',50000)
@@ -27,34 +34,56 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_2.email,'Damba.Kamba@email.com')
 
     def test_fullname(self):
-        emp_1 = Employee('Abba','Sabba',50000)
-        emp_2 = Employee('Hamba','Kamba',60000)
+        # emp_1 = Employee('Abba','Sabba',50000)
+        # emp_2 = Employee('Hamba','Kamba',60000)
 
-        self.assertEqual(emp_1.fullname,'Abba Sabba')
-        self.assertEqual(emp_2.fullname,'Hamba Kamba')
+        self.assertEqual(self.emp_1.fullname,'Abba Sabba')
+        self.assertEqual(self.emp_2.fullname,'Hamba Kamba')
         
-        emp_1.first = 'Dabba'
-        emp_2.first = 'Damba'
+        self.emp_1.first = 'Dabba'
+        self.emp_2.first = 'Damba'
 
-        self.assertEqual(emp_1.fullname,'Dabba Sabba')
-        self.assertEqual(emp_2.fullname,'Damba Kamba')
+        self.assertEqual(self.emp_1.fullname,'Dabba Sabba')
+        self.assertEqual(self.emp_2.fullname,'Damba Kamba')
 
     def test_apply_raise(self):
 
-        emp_1 = Employee('Abba','Sabba',50000)
-        emp_2 = Employee('Hamba','Kamba',60000)
+        # emp_1 = Employee('Abba','Sabba',50000)
+        # emp_2 = Employee('Hamba','Kamba',60000)
         
-        emp_1.apply_raise()
-        emp_2.apply_raise()
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
 
-        self.assertEqual(emp_1.pay, 52500)
-        self.assertEqual(emp_2.pay,63000)
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay,63000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get: 
+            #Using the context manager and we want to mock requests.get , the object of employee
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text= 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('https://company.com/Abba/May')
+            self.assertEqual(schedule,'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('https://company.com/Hamba/June')
+            self.assertEqual(schedule,'Bad Response!')
+
+
         
 if __name__ =='__main__':
     unittest.main()
  
 
 
+#Note : The tests should be isolated means your test should not be rely on the other test or affect the 
+# other test 
+# Test Driven Development : Write the test before the code , means Think about your code that what you 
+#want to do and than write the test to implement behaviour and test fails then fix this 
 
 
 
