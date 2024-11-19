a8d521fd1760d98ab544f4d3162b4b714bd77840
= <=>
2024-11-19 23:56:46 +0530

    update

--- Python3/Functional_Programming/functional_programming.py
+++ Python3/Functional_Programming/functional_programming.py
@@ -1,39 +1,45 @@
+#----------------------------------------------------------------------#
+#                            FUNCTIONS
+##----------------------------------------------------------------------#
+
 '''Function take inputs and gives output 
 
 2 Types of functions: 
-1. Traditional
-
+1. Traditional 2. Pure Functions
+'''
+#----------------------------------------------------------------------------------------#
+#                            1. Traditional Functions
+#----------------------------------------------------------------------------------------#
+'''
 -> Function programming utilies the function for the clean ,consitent and maintainable code 
 
 -> functional programming is differntiate by design comapare to object oriented.
--> does not change the data outside the scope of the function , its only returned the function result
+-> does not change the data outside the scope of the function ,
+ its only returned the function result
 that is being called .
 
-. the function is being considered as a standalone and independet .
+> the function is being considered as a standalone and independet .
 
 -> Many object oriented programming langauges has aided  the functional programming.
--> In python functions are called first class citizen ,Means they have same level of functions , strings and Numbers 
+-> In python functions are called first class citizen ,
+Means they have same level of functions , strings and Numbers 
 
--> fun.can be assign to varriable   passed as an argument  and returned can returned as its caller.
+-> fun.can be assign to varriable  passed as an argument 
+ and returned can returned as its caller.
 
 -> In function programming the logic  for certain tasks is already built in and function can 
 be resuable , and save a lot of development time. 
 
-
-Note: :: slice function that is used to include the list item after the index value or before the index Value
+Example: :: slice function that is used to include the list item after the index value or before the index Value
 like ::-1 and :: 1
 
 '''
-
+#----------------------------------------------------------------------------------------#
+#                                 2. Pure functions : 
+#----------------------------------------------------------------------------------------#
 '''
-2. Pure functions : 
 It is the function that does not have the data list or function and can't afftect beyond 
 it's scope .
-
-def add_list(lst,item):
-n1 = lst.copy()
-n1.append(item)
-return n1
 
 * Know the outcome
 * Consistent and reliable
@@ -42,20 +48,26 @@ return n1
 * pure function prevents changes on global scopes 
 
 '''
+def add_list(lst,item):
+    n1 = lst.copy()
+    n1.append(item)
+    return n1
+list = add_list([1,2,3,4],6)
+print(list)
 
-# Altering the Pure Functions
+#----------------------------------------------------------------------------------------#
+#                   Altering the Pure Functions
+#----------------------------------------------------------------------------------------#
 
 my_list = [1,2,3,4]
 def  add_list(item):
     return my_list.append(item)
 
-add_list(4)
+add_list(5)
 
 print(my_list) # data is manipulated at the global stage
-
-
-#alternig the function 
-
+#----------------------------------------------------------------------------------------#
+#2.
 my_list = [1,2,3,4]
 def  add_list(item):
     my_list.append(item)
@@ -66,18 +78,35 @@ add_list(4)
 print(my_list)
 print(new_list)
 
+#----------------------------------------------------------------------------------------#
+#                                alternig the function 
+#----------------------------------------------------------------------------------------#
 #:Pure Function
 
 my_list1 = [1,2,3,4,5]
 
-def add_list(lst,item):
-    new_list= lst.copy # new list creating the copy 
-    #new_list.append(item)
+def add_list(lst1,item1):
+# append the item in the new list
+    #new_list= my_list.copy()
+    new_list = lst1.copy()
+    print("This is new list ",new_list) # original list 
+    new_list.append(item1)
     return new_list
 
-new_list =add_list(my_list1,6)
-print(new_list) # original list 
+append_list= add_list([1,2,3,4],6)
+print("this is appended list", append_list)
 print(my_list1) #here pure fucntion doesn't change the original list at global scope
 
-
+#----------------------------------------------------------------------------------------#
+#         Calling the function differnt types
+#----------------------------------------------------------------------------------------#
+def add_sub(a,b):     # takes arguments or param
+    c =a+b
+    d = a-b
+    print(c)
+    print(d)
+    return c,d 
+add_sub(5,4)
+r1,r2 = add_sub(9,2)
+result = add_sub(6,4)  # when using the object the function Expecting someting to return
 
