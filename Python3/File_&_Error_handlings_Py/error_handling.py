b455a18de549ca828efb7c6e46473c21c9e5a574
= <=>
2024-11-25 00:08:27 +0530

    Update

--- Python3/File_&_Error_handlings_Py/error_handling.py
+++ Python3/File_&_Error_handlings_Py/error_handling.py
@@ -1,22 +1,63 @@
+#-------------------------------------------------------------------------------------------------------#
+#                                        Error Handlings
+#-------------------------------------------------------------------------------------------------------#
+ 
+#. Types of the Error 
 
+'''
+1.Synatx erorr  : or compile time error ; it is easy to found
+,misspelling or typo , Identation error 
 
-'''1. Synatx erorr  : misspelling or typo
-       Identation error 
+2. Logical Error - --> gives Wrong ouput 
+3. Runtime Error ----> code compiles and no logical error but the wrong input is given so 
+it gives the error like: Zero divison error 
+'''
+#-------------------------------------------------------------------------------------------------------#
+#  Exception handling Error:
+#-------------------------------------------------------------------------------------------------------#
 
-
-Exception handling Error: 
-
+''' 
 These are occurr during the execution and needs to be handled
 
 Errors are unavoidable in programming and in Python, it is no different, these needs to be handled
 
 developer need to deal with the issue and keep from failing
 '''
+#-------------------------------------------------------------------------------------------------------#
+#                               Handling the Zero Divison Errors
+#-------------------------------------------------------------------------------------------------------#
+
+#-------------------------------------------------------------------------------------------------------#
+a=5
+b=4 # when b=0 it will give the error 
+print(a/b)
+print("Done")
+
+#1. Handling the above error and executing the rest of the code
+#-------------------------------------------------------------------------------------------------------#
 
 
+def divide_by(a,b):
+    return a/b
+try:
+    ans= divide_by(10,0)
+except ZeroDivisionError as e:
+    print(e, "we cant divide by the zero") # here e is the just represntation of the object of exception
+except Exception as e: 
+    print(e,"something went worng") 
+    
+# handle more than one exception by chaining the except statement by adding another except statement. 
+#zero divison error
+# OR
+
+#2.
+#-------------------------------------------------------------------------------------------------------#
+
+a=5
 def divide_fun(a,b):
     return a/b
-#print(divide_fun(40,0)) #calling the fun by the print fun and it returns the a/b
+#print(divide_fun(40,0)) #calling the fun by the print fun and it returns the a/b    
+
 try:
     ans= divide_fun(40,0)
 
@@ -30,29 +71,8 @@ except Exception as e:
     #so E vaariable acts as a alias for the exceptions
     #means e can print hte content of the error message
 
+# 3.
 
-#or
-def divide_by(a,b):
-    return a/b
-try:
-    ans= divide_by(10,0)
-except ZeroDivisionError as e:
-    print(e, "we cant divide by the zero")
-except Exception as e: 
-    print(e,"something went worng") 
-    
-# handle more than one exception by chaining the except statement by adding another except statement. 
-#zero divison error
-    
-# Starter code
-try:
-    with open('file_does_not_exist.txt', 'r') as file:
-
-        print(file.read())
-except Exception as e:
-    print("The file could not be found ")    
-
-# Starter code
 def divide_by(a, b):
     return a / b
 
@@ -63,8 +83,8 @@ except ZeroDivisionError as e:
     print("Can't divide by the zero:",e)
 
 
-
-# also zero divison error
+# Also Zero Division
+#4.  
 def divide_by(a, b):
     try:
         return a / b
@@ -74,7 +94,35 @@ def divide_by(a, b):
         print(e, 'Something went wrong!')
 
 ans = divide_by(10,0)
-print(ans)
+print(ans)    
+
+#-------------------------------------------------------------------------------------------------------#
+#                      File Opening Error
+#-------------------------------------------------------------------------------------------------------#
+
+# 1. Starter code
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+
+        print(file.read())
+except Exception as e:
+    print("The file could not be found ")    
+
+
+#2.
+
+a=5
+b=2
+try:
+    print("resource was opened ")
+    print(a/b)
+except Exception as e:
+    print("Hey  you can't do this action ",e) 
+finally:
+    print("resource was closed ")        # Finally will be executaed always 
+
+#-------------------------------------------------------------------------------------------------------#
+
 
 # Starter code
 items = [1,2,3,4,5]
