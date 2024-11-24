0d0c8954d2743ec0ee08cd69cede77db846bacf2
= <=>
2024-11-24 22:36:15 +0530

    update

--- Python3/OOPs_in_Python/classs_instance_&_objects_py.py
+++ Python3/OOPs_in_Python/classs_instance_&_objects_py.py
@@ -8,6 +8,37 @@
 #Everything  in py is a object and derived from the object class 
 
 #class create the new type of object and from which you can create the instances 
+'''
+Object:
+
+An object is a fundamental concept in OOP. 
+It represents a concrete entity that combines data (attributes) and behavior (methods).
+
+The term "instance" emphasizes the relationship between an object and its class.
+ When we create an object from a class, we say that we "instantiate" the class.
+'''
+
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def display(self):
+        print(f"This car is a {self.make} {self.model}")
+
+#let's create an object of this class:      
+my_car = Car("Toyota", "Corolla")
+my_car.display()  # Output: This car is a Toyota Corolla
+
+'''Car is the class.
+
+my_car is an object of the class Car.
+
+my_car is also an instance of the class Car.
+'''
+#--------------------------------------------------------------------------------------------------------------------#
+#2.
 
 class Myclass:
     #pass    
@@ -40,27 +71,6 @@ print(obj_myclass.a)
 # instance refernece to a class 
 print(obj_myclass.hello_method())
 print(obj_myclass.hello_method1(3))
-
-#--------------------------------------------------------------------------------------------------------------------#         
-#                                               Type of the object :
-#--------------------------------------------------------------------------------------------------------------------#
-
-'''
-class object 
-instance object 
-and 
-Mehtod object                    
-              
-here Myclass is   the class object and the 
-obj_myclass is the instance of the myclass
-'''
-#Note: Classes perform to types of operation  1. attribute refrences   2. isinstansiating
- 
-#Instansitaion process in python involves three key procees 
-# 1. Class Defination
-#2. Creating the new instance 
-#3.  Intitalizing the new instance 
-
 
 #--------------------------------------------------------------------------------------------------------------------#
 #                                  Class and Object Excercise and demostrations : 
@@ -114,5 +124,109 @@ class Example:
 # Creating an instance
 obj = Example(42)
 obj.display_value()  # Output: 42
+
+#--------------------------------------------------------------------------------------------------------------------#
+
+
+#--------------------------------------------------------------------------------------------------------------------#         
+#                                               Type of the object :
+#--------------------------------------------------------------------------------------------------------------------#
+
+'''
+class object 
+instance object 
+and 
+Mehtod object                    
+              
+here Myclass is   the class object and the 
+obj_myclass is the instance of the myclass
+'''
+#Note: Classes perform to types of operation  1. attribute refrences   2. isinstansiating
+ 
+#Instansitaion process in python involves three key procees 
+# 1. Class Defination
+#2. Creating the new instance 
+#3.  Intitalizing the new instance 
+
+#1.  Instance Objects:
+# These are the most common type of objects and represent individual instances of a class.
+#  Each instance object has its own set of instance variables.
+
+#--------------------------------------------------------------------------------------------------------------------#
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
+
+# 2. Class Objects:
+# The class itself is also an object.
+#  It is an instance of the type metaclass in Python and defines the blueprint for creating instance objects.
+
+# Class variables (attributes) are shared across all instances of the class.
+
+
+class Car:
+    number_of_wheels = 4  # Class variable
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+print(Car.number_of_wheels)  # Accessing class variable
+
+# 3. Static Method Objects:
+# These methods belong to the class rather than any instance. 
+# They don't require access to the instance (self) or the class (cls).
+
+class Car:
+    @staticmethod
+    def is_motor_vehicle():
+        return True
+
+print(Car.is_motor_vehicle())  # Output: True
+
+# 4. Class Method Objects:
+# These methods have access to the class itself via the cls parameter and can modify class state.
+
+
+class Car:
+    number_of_wheels = 4
+
+    @classmethod
+    def set_number_of_wheels(cls, num):
+        cls.number_of_wheels = num
+
+Car.set_number_of_wheels(6)
+print(Car.number_of_wheels)  # Output: 6
+
+# 5. Inner Class Objects:
+
+# A class defined within another class. 
+# Inner classes can help logically group classes that are only used in one place.
+
+
+class OuterClass:
+    class InnerClass:
+        def __init__(self, value):
+            self.value = value
+
+outer_instance = OuterClass()
+inner_instance = OuterClass.InnerClass(42)
+print(inner_instance.value)  # Output: 42
+
+'''Summary:
+In a class, you will typically deal with:
+
+Instance Objects for creating specific instances of the class.
+
+Class Objects for defining shared attributes and methods.
+
+Static and Class Method Objects for methods associated with the class itself.
+
+Inner Class Objects for nested class definitions.'''
 
 #--------------------------------------------------------------------------------------------------------------------#
