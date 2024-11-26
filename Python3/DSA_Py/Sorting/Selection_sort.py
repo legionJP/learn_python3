889ad4caea8e129b3d8c0dd10b756f49067d351d
= <=>
2024-11-27 00:27:38 +0530

    update Selection sort

---
+++ Python3/DSA_Py/Sorting/Selection_sort.py
@@ -0,0 +1,35 @@
+#--------------------------------------------------------------------------------------------------------------#
+#                                Selection  Sort  
+#--------------------------------------------------------------------------------------------------------------#
+
+# in selection find the min value from the start to end -- 
+# so end of the iteration min = find value 
+# than swap min value with the first value ,
+# so comapre every value and swap at ones
+
+
+def select_sort(nums):
+    for i in range(len(nums)-1):
+        minpos = i
+        for j in range(i,len(nums)):
+            if nums[j]< nums[minpos]:
+                minpos = j
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos]= temp
+
+        print(nums)
+
+nums = [87,45,5,22,32,456,12,34]
+select_sort(nums)
+print(nums)
+
+def selection_sort(nums):
+    for i in range(len(nums)-1):
+        minops =i
+        for j in range(i,len(nums)):
+            if nums[j]<nums[minops]:
+                minops=j
+        temp = nums[i]
+        nums[i]= nums[minops]
+        nums[minops]=temp
