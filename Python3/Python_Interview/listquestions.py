7173729465aff150b85cf87de91b1169e814565f
= <=>
2024-12-22 18:11:09 +0530

    Adding the problem to check the sum of two  variables equal to k

---
+++ Python3/Python_Interview/listquestions.py
@@ -0,0 +1,38 @@
+# add two element from the list to check if the sum equals to a variable k
+
+def find_pair_sum(nums, k):
+
+    # # 1. Using the Hash Map 
+    # seen= {}
+    # for num in nums:
+    #     complement = k-num
+    #     if complement in seen:
+    #         return complement , num
+    #     else:
+    #      seen[num] = True
+    # return None
+
+# Logic ==> the complement will be the k-num where it will be c= 15-num = num+c == K, so when each complement is stroed in hastable
+# dict of seen and searching if the complement c = num in the nums list , means the complement of new num is  found in seen then its result
+
+# 2. Using the Two Pointer
+    nums.sort()
+    l , r = 0 ,len(nums)-1
+    while l<r:
+        current_sum = nums[l]+ nums[r]
+        if current_sum == k:
+            return nums[l], nums[r]
+        elif current_sum < k:
+            l += 1
+        else:
+            r -= 1
+    return None
+        
+# Example usage
+nums = [10, 2, 3, 7, 8]
+k = 15
+result = find_pair_sum(nums, k)
+print(result)  # Output: (7, 8)
+
+# first assign the l and r 0 and array -1 to traverse
+# than compare if the 
