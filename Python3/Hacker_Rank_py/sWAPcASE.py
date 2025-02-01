68b115967eb614f478e9d635f37cb3616729c046
legionJP <jjpsingh18@gmail.com>
2025-02-02 01:02:01 +0530

    update

--- Python3/Hacker_Rank_py/sWAPcASE.py
+++ Python3/Hacker_Rank_py/sWAPcASE.py
@@ -12,7 +12,19 @@ def swap_case(s):
         else: 
             result+=i.lower()
     return result
-        
+
+# Using the list comprehension
+def swap_case(s):
+    return ''.join([ i.upper() if i.islower() else i.lower() for i in s])
+
+# return ''.join(
+#     [i.upper() for i in s if i.islower()] +
+#     [i.lower() for i in s if not i.islower()]
+# )
+# [expression for item in iterable if condition]
+# result = [i.upper() for i in s if i.islower()] + [i.lower() for i in s if i.isupper()]
+# final_result = ''.join(result)
+
         
 if __name__ == '__main__':
     s = input()
